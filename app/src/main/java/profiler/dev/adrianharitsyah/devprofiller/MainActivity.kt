9e8f7a0ef0454ba99065c4149fd221a8b3913847
Adrian Haritsyah <adrian.haritsyah@gmail.com>
2018-02-15 13:10:57 +0700

    inital commit

---
+++ app/src/main/java/profiler/dev/adrianharitsyah/devprofiller/MainActivity.kt
@@ -0,0 +1,12 @@
+package profiler.dev.adrianharitsyah.devprofiller
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
